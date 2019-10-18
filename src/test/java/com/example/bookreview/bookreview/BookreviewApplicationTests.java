e0a6d57edac0756ada9a893dba82de39029598d8
siddharthconnekt <56437151+siddharthconnekt@users.noreply.github.com>
2019-10-18 12:56:01 +0530

    second change

--- src/test/java/com/example/bookreview/bookreview/BookreviewApplicationTests.java
+++ src/test/java/com/example/bookreview/bookreview/BookreviewApplicationTests.java
@@ -1,21 +1,14 @@
 package com.example.bookreview.bookreview;
 
 import java.util.List;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import com.example.books.entity.Book;
 import com.example.books.service.BookService;
-
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class BookreviewApplicationTests {
+	
 	@Autowired
 	private BookService bookService;
 	
